474240f7af4f66faef737baf4eb644d219b250e3
cassio-morais <cassiomoraisg@hotmail.com>
2020-09-10 12:53:56 -0300

    first

---
+++ VideoToGif/Program.cs
@@ -0,0 +1,34 @@
+﻿using FFmpeg.NET;
+using System;
+using System.Threading.Tasks;
+
+namespace VideoToGif
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        
+        {
+            var video = @"D:\ffmpeg\bin\dogs.mp4";
+
+            await Program.Converter(video);
+            Console.WriteLine("completo!");
+
+        }
+
+        internal static async Task Converter(string inputFile)
+        {
+
+            var splitFile = inputFile.Split(".");
+            var outputFile = splitFile[0] + splitFile[1].Replace(splitFile[1], ".gif");
+
+            var inputVideo = new MediaFile(inputFile); // arquivo origem
+            var outputGif = new MediaFile(outputFile); // arquivo destino
+
+            var ffmpeg = new Engine(@"D:\ffmpeg\bin\ffmpeg.exe"); // aponta o conversor
+
+            await ffmpeg.ConvertAsync(inputVideo, outputGif);
+
+        }
+    }
+}
